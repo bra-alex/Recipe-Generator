0c6dc52954a4e60b65866663dd228aa98f6d76ce
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-02-28 19:34:53 +0000

    Initial Commit

---
+++ Recipe-Generator/Recipe_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_GeneratorApp.swift
+//  Recipe-Generator
+//
+//  Created by Don Bouncy on 28/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
